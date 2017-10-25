655ce800a838e603f0b1e5230b9f24abb66f8742
JohnShone94 <jshone94@yahoo.com>
2017-10-25 14:22:26 +0100

    Commit 0.0.1

---
+++ Source/TheMainGame/TheMainGameGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "TheMainGameGameMode.h"
+#include "TheMainGameHUD.h"
+#include "TheMainGameCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ATheMainGameGameMode::ATheMainGameGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ATheMainGameHUD::StaticClass();
+}
