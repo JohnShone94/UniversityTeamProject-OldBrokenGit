2f33dc2f476423e3e1b2526fdf5225ec1dd324bc
JohnShone94 <jshone94@yahoo.com>
2017-11-17 23:12:49 +0000

    Commit 4.0.0
    Adding a base pickup file

---
+++ Source/TheMainGame/Pickup.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pickup.generated.h"
+
+UCLASS()
+class THEMAINGAME_API APickup : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickup();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	FORCEINLINE class UStaticMeshComponent* GetMesh() const { return PickupMesh; }
+
+	UFUNCTION(BlueprintPure, Category = "Pickup")
+		bool IsActive();
+
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+		void SetActive(bool active);
+
+	UFUNCTION(BlueprintNativeEvent)
+		void WasCollected();
+		virtual void WasCollected_Implementation();
+
+
+protected:
+	bool bIsActive;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Pickup", meta = (AllowPrivateAccess = "true"))
+		class UStaticMeshComponent* PickupMesh;
+};
