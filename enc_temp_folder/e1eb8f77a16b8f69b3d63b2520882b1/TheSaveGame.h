c7d504407dd67539c62cd91898b9e50651cbfcaa
JohnShone94 <jshone94@yahoo.com>
2018-03-03 02:50:01 +0000

    Quite a few changes

---
+++ enc_temp_folder/e1eb8f77a16b8f69b3d63b2520882b1/TheSaveGame.h
@@ -0,0 +1,56 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "TheSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEMAINGAME_API UTheSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+
+	UTheSaveGame();
+
+public:
+
+	UPROPERTY(EditAnywhere, Category = "Save Game")
+		FString SaveSlotName;
+
+	UPROPERTY(EditAnywhere, Category = "Save Game")
+		uint32 UserIndex;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		int sCurrentPower;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		int sMaxPower;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		int sHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		int sMaxHealth;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		FName sSpawnPoint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		bool sPortalActive;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		FName sWorldName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		int sTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		FVector location;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Save Game")
+		bool sOffWorld;
+};
